35836ab5c5d50f15f39b826a818fe9f378e4b5e3
SMPlay <pavlik.sokolov.2000@gmail.com>
2020-05-18 11:58:59 +0300

    rename js to jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => (
+  <header>
+    <nav>
+      <ul>
+        <li>Li 1</li>
+        <li>Li 2</li>
+        <li>Li 3</li>
+        <li>Li 4</li>
+        <li>Li 5</li>
+      </ul>
+    </nav>
+  </header>
+);
+
+export default Header;
