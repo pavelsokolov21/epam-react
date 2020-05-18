42becdbed6c76a833aca5cb79c799c6e62ed3f47
SMPlay <pavlik.sokolov.2000@gmail.com>
2020-05-18 11:12:30 +0300

    added conf for scss and img. Added eslint

---
+++ build/webpack.base.config.js
@@ -0,0 +1,122 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+const PATHS = {
+  src: path.join(__dirname, "../src"),
+  dist: path.join(__dirname, "../dist"),
+  public: path.join(__dirname, "../public")
+};
+
+module.exports = {
+  externals: {
+    paths: PATHS,
+  },
+  entry: {
+    app: `${PATHS.src}/index.js`,
+  },
+  output: {
+    filename: "[name].[hash].bundle.js",
+    path: `${PATHS.dist}`,
+  },
+
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          name: "vendors",
+          test: /node_modules/,
+          chunks: "all",
+          enforce: true,
+        },
+      },
+    },
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: { sourceMap: true },
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              sourceMap: true,
+              config: { path: "./postcss.config.js" },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: { sourceMap: true },
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              sourceMap: true,
+              config: { path: "./postcss.config.js" },
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: { sourceMap: true },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg|otf)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader',
+        options: {
+            name: '[name].[ext]'
+        }
+    },
+    ],
+  },
+
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].[hash].css",
+    }),
+    new HtmlWebpackPlugin({
+      hash: true,
+      template: `${PATHS.public}/index.html`,
+      filename: `${PATHS.dist}/index.html`,
+    }),
+  ],
+};
---
+++ build/webpack.dev.config.js
@@ -0,0 +1,25 @@
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const baseWebpackConfig = require('./webpack.base.config');
+
+const devWebpackConfig = merge(baseWebpackConfig, {
+    mode: 'development',
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        port: 8081,
+        contentBase: baseWebpackConfig.externals.paths.dist,
+        overlay: {
+            warnings: false,
+            errors: true
+        }
+    },
+    plugins: [
+        new webpack.SourceMapDevToolPlugin({
+            filename: '[file].map'
+        })
+    ]
+});
+
+module.exports = new Promise((resolve, reject) => {
+    resolve(devWebpackConfig);
+});
