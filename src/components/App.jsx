b2a08ddf504388add47e3a22e6ec85ca55077f24
SMPlay <pavlik.sokolov.2000@gmail.com>
2020-05-24 17:17:40 +0300

    added Error Boundary

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,15 +4,18 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomePage from "containers/pages/HomePage";
 import CurrentFilmPage from "containers/pages/CurrentFilmPage";
 import Footer from "containers/Footer";
+import ErrorBoundary from "./ErrorBoundary";
 import MainState from "../context/mainState";
 
 const App = () => (
   <Router>
     <Switch>
-      <MainState>
-        <Route path="/" exact component={HomePage} />
-        <Route path="/movies/:id" exact component={CurrentFilmPage} />
-      </MainState>
+      <ErrorBoundary>
+        <MainState>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/movies/:id" exact component={CurrentFilmPage} />
+        </MainState>
+      </ErrorBoundary>
     </Switch>
     <Footer />
   </Router>
---
+++ src/components/ErrorBoundary.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+export default class ErrorBoundary extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      hasError: false,
+    };
+  }
+
+  componentDidCatch() {
+    this.setState({ hasError: true });
+  }
+
+  render() {
+    const { children } = this.props;
+    const { hasError } = this.state;
+    if (hasError) {
+      return <div>Произошла ошибка :(</div>;
+    }
+
+    return children;
+  }
+}
+
+ErrorBoundary.propTypes = {
+  children: PropTypes.func.isRequired,
+};
