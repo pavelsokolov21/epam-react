6486755436cd51c6304ee653d2a9c7137d61cb56
SMPlay <pavlik.sokolov.2000@gmail.com>
2020-06-02 20:28:43 +0300

    added 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CurrentFilmsPage from "./pages/CurrentFilmPage";
+import NoMatch from "./pages/NoMatch";
 import "./App.css";
 import ErrorBoundary from "./components/ErrorBoundary";
 
@@ -11,6 +12,7 @@ const App = () => (
       <ErrorBoundary>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/movies/:id" component={CurrentFilmsPage} />
+        <Route exact component={NoMatch} />
       </ErrorBoundary>
     </Switch>
   </Router>
